bfa7440bcfe1ec7263d5fc9ca159bb0572523f15
AbubakrRehman <abubakr.rehman91@outlook.com>
2025-01-28 15:32:23 +0530

    add one more js file

--- log.js
+++ log.js
@@ -3,3 +3,8 @@ console.log("this is log.js")
 export default function log(text) {
     console.log(text);
 }
+
+let i = 0;
+while (i < 100000000) {
+    console.log("value of i", i++);
+}
---
+++ log2.js
@@ -0,0 +1,5 @@
+console.log("this is log2.js")
+
+export default function log(text) {
+    console.log(text);
+}
